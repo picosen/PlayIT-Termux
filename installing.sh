8d981f8c6af7a2766e495769772a05f717fd3cd0
PicoSen <96994029+picosen@users.noreply.github.com>
2023-05-07 20:12:43 +0800

    [+] Added ca-certificates

--- installing.sh
+++ installing.sh
@@ -5,4 +5,6 @@ echo "Installing gnupg1"
 apt install gnupg1 -y
 echo "Installing gnupg2"
 apt install gnupg2 -y
+echo "Installing ca-certificates"
+apt install ca-certificates
 curl https://raw.githubusercontent.com/DevPoli/PlayIT-Termux/main/install.sh | bash
